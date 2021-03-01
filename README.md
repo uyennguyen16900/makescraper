776564ca1e11fec182297a68aaacf7561f2aefab
uyennguyen16900 <uyennguyen16900@gmail.com>
2021-03-01 00:08:18 -0800

    Scraped data to file output.json

--- README.md
+++ README.md
@@ -43,9 +43,9 @@ _Complete each task in the order they appear. Use [GitHub Task List](https://hel
 #### Scraping
 
 - [ ] **IMPORTANT**: Complete the Web Scraper Workflow worksheet distributed in class.
-- [ ] Create a `struct` to store your data.
-- [ ] Refactor the `c.OnHTML` callback on line `16` to use the selector(s) you tested while completing the worksheet.
-- [ ] Print the data you scraped to `stdout`.
+- [x] Create a `struct` to store your data.
+- [x] Refactor the `c.OnHTML` callback on line `16` to use the selector(s) you tested while completing the worksheet.
+- [x] Print the data you scraped to `stdout`.
 
 ##### Stretch Challenges
 
@@ -53,8 +53,8 @@ _Complete each task in the order they appear. Use [GitHub Task List](https://hel
 
 #### Serializing & Saving
 
-- [ ] Serialize the `struct` you created to JSON. Print the JSON to `stdout` to validate it.
-- [ ] Write scraped data to a file named `output.json`.
+- [x] Serialize the `struct` you created to JSON. Print the JSON to `stdout` to validate it.
+- [x] Write scraped data to a file named `output.json`.
 - [ ] **Add, commit, and push to GitHub**.
 
 ## Resources
--- scrape.go
+++ scrape.go
@@ -1,30 +1,72 @@
 package main
 
 import (
+	"encoding/csv"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
 
 	"github.com/gocolly/colly"
 )
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
+type Crypto struct {
+	Name   string
+	Price  string
+	Symbol string
+}
+
 func main() {
+	fName := "cryptocoinmarketcap.csv"
+	file, err := os.Create(fName)
+	if err != nil {
+		log.Fatalf("Cannot create file %q: %s\n", fName, err)
+		return
+	}
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	// Write CSV header
+	writer.Write([]string{"Name", "Symbol", "Price (USD)"})
+
 	// Instantiate default collector
 	c := colly.NewCollector()
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+	var coins []Crypto
+	// var jsonText = []byte(`[
+	//     {"Name": "", "Symbol": "", "Price": ""}
+	// ]`)
 
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	c.OnHTML("tbody", func(e *colly.HTMLElement) {
+		e.ForEach("tr", func(_ int, el *colly.HTMLElement) {
+			crypto := Crypto{
+				Name:   el.ChildText("td:nth-child(2)"),
+				Symbol: el.ChildText("td:nth-child(3)"),
+				Price:  el.ChildText(".price___3rj7O"),
+			}
+
+			writer.Write([]string{
+				crypto.Name,
+				crypto.Symbol,
+				crypto.Price,
+			})
+
+			// if err := json.Unmarshal([]byte(jsonText), &coins); err != nil {
+			// 	log.Println(err)
+			// }
+			coins = append(coins, crypto)
+
+		})
+		cryptoJSON, _ := json.MarshalIndent(coins, "", "")
+		fmt.Println(cryptoJSON)
+		_ = ioutil.WriteFile("output.json", cryptoJSON, 0644)
 	})
 
-	// Before making a request print "Visiting ..."
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL.String())
-	})
+	c.Visit("https://coinmarketcap.com/all/views/all/")
 
-	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	log.Printf("Scraping finished, check file %q for results\n", fName)
 }
